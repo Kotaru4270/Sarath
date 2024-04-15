1894f2becd2c3db23756943790527861bb54f160
Kotaru4270 <156743504+Kotaru4270@users.noreply.github.com>
2024-04-15 20:44:57 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/**
+ * Add event listener on multiple elements
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+/**
+ * Navbar toggle for mobile
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * Header
+ * Active header when window scroll down to 100px
+ */
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+/**
+ * Scroll reveal
+ */
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+/**
+ * Smooth scrolling to sections
+ */
+const scrollToSection = function (sectionId) {
+  const section = document.getElementById(sectionId);
+  window.scrollTo({
+    top: section.offsetTop - header.clientHeight, // Adjusting for header height
+    behavior: "smooth"
+  });
+}
+
+// Linking navigation items to their respective sections
+document.querySelectorAll('.navbar-link').forEach(function(link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const sectionId = link.getAttribute('href').substring(1);
+    scrollToSection(sectionId);
+  });
+});
